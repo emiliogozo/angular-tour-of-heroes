7543181ee2d2780cc5f0e59b491edf09be6f71ec
Emilio Gozo <emilio.gozo@obf.ateneo.edu>
2016-12-24 11:15:43 +0800

    feat: create a new Hero

--- app/heroes.component.ts
+++ app/heroes.component.ts
@@ -8,17 +8,17 @@ import { HeroService } from './hero.service';
   moduleId: module.id,
   selector: 'my-heroes',
   templateUrl: 'heroes.component.html',
-  styleUrls: [ 'heroes.component.css' ]
+  styleUrls: ['heroes.component.css']
 })
 
-export class HeroesComponent implements OnInit{
+export class HeroesComponent implements OnInit {
   heroes: Hero[];
   selectedHero: Hero;
 
   constructor(
     private router: Router,
     private heroService: HeroService) { }
-  
+
   getHeroes(): void {
     this.heroService.getHeroes().then(heroes => this.heroes = heroes);
   }
@@ -34,4 +34,15 @@ export class HeroesComponent implements OnInit{
   gotoDetail(): void {
     this.router.navigate(['/detail', this.selectedHero.id]);
   }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.create(name)
+      .then(hero => {
+        this.heroes.push(hero);
+        this.selectedHero = null;
+      });
+  }
+
 }
